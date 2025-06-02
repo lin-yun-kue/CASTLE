6f80bfc2e00f5f3ca114fbd8c921b0f963d8e94d
wel381 <grace.weiqi.li@gmail.com>
2025-06-02 01:02:06 -0700

    add debug plots

--- preprocessing.py
+++ preprocessing.py
@@ -18,7 +18,7 @@ samples = ["breast_g1"]
 for sample in samples:
     ## gene expression--------------------------------------------------
     # need to rewrite h5ad file to a format that geneformer can tokenize
-    selected_id = util.process_raw_expression(sample, thres = 500)
+    selected_id = util.process_raw_expression(sample, thres = 200)
 
     from_dir = os.path.join(raw_dir, sample)
     to_dir = os.path.join(processed_dir, sample)
@@ -33,6 +33,8 @@ for sample in samples:
 
     # spatial coordinates
     coords = util.process_raw_coord(from_dir, selected_id)
+
+    torch.save(coords, os.path.join(to_dir, "raw_coords.pth")) # raw
     spatial_extractor = SpatialExtractor2D()
     spatial_extractor.encode(coords, to_dir)
 
@@ -60,8 +62,6 @@ for sample in samples:
     gt = dict(zip(gt["Barcode"], gt["Cluster"]))
     selected_gt = [gt.get(id, -1) for id in selected_id]
     torch.save(torch.tensor(selected_gt, dtype = torch.int8), os.path.join(to_dir, "ground_truth.pth"))
-
-
 
 
 with h5py.File(os.path.join(from_dir,"cell_feature_matrix.h5"), "r") as f:
